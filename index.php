bad6763df26219f5292976ed9015e04c35bf86be
khauanecamozzatto <khauane.camozzatto.silva@escola.pr.gov.br>
2024-08-16 08:36:38 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<title>barbearia Alura</title>
+<LINK REL"stylesheet" hre="style.css">
+<style>
+
+
+</head>
+<body>
+
+    <h1 style="text-align:center">sobre a barbearia Alura</h1>
+ <p>Localizada no coração da cidade a <strong>Barbearia Alura</
+ strong> traz para o mercado o que há de melhor para o seu cabelo
+  e barba.fundada em 2019, a barbearia Alura já é destaque na cidade e conquista novos clientes a cada dia.</p>
+
+<p style="font-size:20px">/strong"proporcionar auto-estima e qualidade de vida aos clientes 
+    de vida aos clientes a cada dia.</p>
+
+    <p>Oferecemos profissionais experientes e antenados ás mudanças no mundo da moda.O atendimento possui padrão de exelência e agilidade,grantido qualidade e satisfação dos nossos clientes.</p>
+
+
+
+    </body>
+
+
+    </html/>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</title>
